58386b98924eeb70f5ad965e6747353d9f8f79e1
Anca Nechita <anca.nechita.moldavia@gmail.com>
2026-02-07 12:45:49 +0200

    Add login tests using Page Object Model

---
+++ fixtures/LoginData.ts
@@ -0,0 +1,10 @@
+export const validUser = {
+                  username: 'standard_user',
+                  password: 'secret_sauce'
+
+}
+
+export const invalidUser = { 
+                  username: 'regular_user',
+                  password: 'known_password'
+}
---
+++ tests/testLogin.spec.ts
@@ -0,0 +1,49 @@
+
+import { test, expect, Page } from '@playwright/test'
+import { LoginPage } from '../pages/LoginTest'
+import { validUser, invalidUser } from '../fixtures/LoginData'
+
+
+test.describe('Login Tests', () => {
+ test.beforeEach (async ({ page }) => {
+ await page.goto('https://www.saucedemo.com/')
+ await expect(page).toHaveURL('https://www.saucedemo.com/')
+})
+test('Login with valid credentials', async ({ page }) => {
+  const loginPage = new LoginPage(page)
+  await loginPage.fillUsernameInput(validUser.username)
+  await loginPage.fillPasswordInput(validUser.password)
+  await loginPage.clickLoginButton()
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+})
+
+test('Login with invalid username', async ({ page }) => {
+
+  const loginPage = new LoginPage(page)
+
+  await loginPage.fillUsernameInput(invalidUser.username)
+  await loginPage.fillPasswordInput(validUser.password)
+  await loginPage.clickLoginButton()
+  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service')
+                  
+})
+test('Login with invalid password', async ({ page }) => {
+
+  const loginPage = new LoginPage(page)
+
+  await loginPage.fillUsernameInput(validUser.username)
+  await loginPage.fillPasswordInput(invalidUser.password)
+  await loginPage.clickLoginButton()
+  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service')
+                  
+})
+test('Login with empty fields', async ({ page }) => {
+
+  const loginPage = new LoginPage(page)
+
+  await loginPage.fillUsernameInput('')
+  await loginPage.fillPasswordInput('')
+  await loginPage.clickLoginButton()
+  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username is required')
+})
+})
