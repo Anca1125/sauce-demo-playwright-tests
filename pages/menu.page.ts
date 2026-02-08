8dca36e70e773a4cb3519fabfc4cab8110e678e8
Anca Nechita <anca.nechita.moldavia@gmail.com>
2026-02-08 10:44:48 +0200

    Complete menu page tests

--- pages/menu.page.ts
+++ pages/menu.page.ts
@@ -8,6 +8,10 @@ export class MenuPage {
   readonly logoutLink: Locator;
   readonly resetAppStateLink: Locator;
   readonly closeMenuButton: Locator;
+  readonly productsList: Locator;
+  readonly shoppingCart: Locator;
+  readonly cartBadge: Locator;
+  readonly filterItems: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -17,5 +21,19 @@ export class MenuPage {
     this.logoutLink = page.locator("#logout_sidebar_link");
     this.resetAppStateLink = page.locator("#reset_sidebar_link");
     this.closeMenuButton = page.locator("#react-burger-cross-btn");
+    this.productsList = page.locator(".inventory_item");
+    this.shoppingCart = page.locator('[data-test="shopping-cart-link"]');
+    this.cartBadge = page.locator(".shopping_cart_badge");
+    this.filterItems = page.locator(".product_sort_container");
+  }
+
+  addToCartButton(productName: string) {
+    return this.page.locator(`[data-test="add-to-cart-${productName}"]`);
+  }
+  removeFromTheCartButton(productName: string) {
+    return this.page.locator(`[data-test="remove-${productName}"]`);
+  }
+  productTitle(productName: string) {
+    return this.page.locator(`.inventory_item_name:has-text("${productName}")`);
   }
 }
